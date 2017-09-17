1cf6c0e63669eb6d8ae4d0160e44b659df16275e
Isabel Lee <ijl0322@gmail.com>
2017-09-17 13:49:21 -0400

    start implementing cat select

---
+++ CatJumpGame/CatJumpGame/CatSelectionScene.swift
@@ -0,0 +1,17 @@
+//
+//  CatSelectionScene.swift
+//  CatJumpGame
+//
+//  Created by Isabel  Lee on 17/09/2017.
+//  Copyright © 2017 isabeljlee. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class CatSelectionScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+
+}
